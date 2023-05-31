f8e57b52f99d428c8c83786ecbbf45868228d102
staticclown <george38691@gmail.com>
2023-05-31 20:53:37 +0530

    added teacher

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    t_name=models.CharField(max_length=15)
+    gender=models.CharField(max_length=2)
+    t_id=models.CharField(max_length=10,primary_key=True)
+    qualifications=models.CharField(max_length=20)
+    password=models.CharField(max_length=10)
+    year=models.DateField()
+    
