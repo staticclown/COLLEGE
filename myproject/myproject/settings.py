d46af27cb203a3aa4f13c777326be4c04ddeb181
staticclown <george38691@gmail.com>
2023-05-31 20:05:57 +0530

    updated database

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "myapp",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":"college_back",
+        'HOST':'127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'1234',
+        'PORT':'3306',
+        
     }
 }
 
