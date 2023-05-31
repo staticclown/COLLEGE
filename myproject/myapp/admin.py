3e14bdf8f76f40d09f8fd62b50254b199f89bb0e
staticclown <george38691@gmail.com>
2023-05-31 22:22:03 +0530

    added dep,subject table

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Teacher
+from .models import Teacher,Subject,Department
 
 admin.site.register(Teacher)
+admin.site.register(Department)
+admin.site.register(Subject)
 
 # Register your models here.
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
-    t_name=models.CharField(max_length=15)
+    tname=models.CharField(max_length=15)
+    tmail=models.CharField(max_length=15,default=None)
     gender=models.CharField(max_length=2)
-    t_id=models.CharField(max_length=10,primary_key=True)
+    tid=models.CharField(max_length=10,primary_key=True)
     qualifications=models.CharField(max_length=20)
     password=models.CharField(max_length=10)
     year=models.DateField()
+
+class Department(models.Model):
+    depid=models.CharField(max_length=15,primary_key=True)
+    depname=models.CharField(max_length=10,default=None)
+
+class Subject(models.Model):
+    subid=models.CharField(max_length=15,primary_key=True)
+    subname=models.CharField(max_length=15)
+    sem=models.CharField(max_length=10)
     
