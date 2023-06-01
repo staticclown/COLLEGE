25911453caf0bbcdd8457110feeed653ae63a40c
staticclown <george38691@gmail.com>
2023-06-01 21:00:31 +0530

    update

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,22 +1,20 @@
 from django.db import models
-
 # Create your models here.
-class Teacher(models.Model):
-    tname=models.CharField(max_length=15)
-    tmail=models.CharField(max_length=15,default=None)
-    gender=models.CharField(max_length=2)
-    tid=models.CharField(max_length=10,primary_key=True)
-    qualifications=models.CharField(max_length=20)
-    password=models.CharField(max_length=10)
-    year=models.DateField()
-
 class Department(models.Model):
     depid=models.CharField(max_length=15,primary_key=True)
     depname=models.CharField(max_length=10,default=None)
-    HODnanme=models.CharField(max_length=15,default=None)
+    HODname=models.CharField(max_length=15,default=None)
 
 class Subject(models.Model):
     subid=models.CharField(max_length=15,primary_key=True)
     subname=models.CharField(max_length=15)
     sem=models.CharField(max_length=10)
-    
+class Teacher(models.Model):
+    tname=models.CharField(max_length=15)
+    tmail=models.CharField(max_length=15,default=None)
+    gender=models.CharField(max_length=10)
+    tid=models.CharField(max_length=10,primary_key=True)
+    qualifications=models.CharField(max_length=20)
+    password=models.CharField(max_length=10)
+    year=models.DateField()
+    depid=models.ForeignKey(Department, on_delete=models.CASCADE,default='D0001') 
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def fun1(request):
+    return HttpResponse('hi')
