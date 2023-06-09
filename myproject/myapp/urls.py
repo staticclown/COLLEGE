d00d16bf0a11c60beda0895aa063e3054aea1434
staticclown <george38691@gmail.com>
2023-06-09 12:08:05 +0530

    update

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addstaff/',views.TeacherView.as_view()),
+    path('viewteacher/',views.TeacherView.as_view()),
     path('subject/',views.SubjectView.as_view()),
     path('login/',views.LoginView.as_view()),
 ]
